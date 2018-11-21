687d29d51257093973ed31612eb23085e7322afb
Ren Wang <iamwrm@gmail.com>
2018-11-21 15:44:08 +0800

    exmem

--- pipeline.v
+++ pipeline.v
@@ -73,13 +73,18 @@ module pipeline(
 	);
 
 
+	wire [31:0] sign_extend__out__data;
+	Sign_Extend sign_extend(.small_In(if_id__out__ins[15:0]),
+		.big_Out(sign_extend__out__data)
+	);
+
+
+	assign reg_file__in__read_addr_1 = if_id__out__ins[25:21];
+    	assign reg_file__in__read_addr_2 = if_id__out__ins[20:16];
+
+
 	wire reg_file__in__RegWrite;
 	wire [4:0] reg_file__in__read_addr_1, reg_file__in__read_addr_2,reg_file__in__write_addr;
-	// module Reg_File(input clk ,RegWrite,
-	//               input [4:0] ReadRegister1, ReadRegister2,WriteReg,
-	//        	        input [31:0] WriteData,
-	//        	        output wire [31:0] read_data1, read_data2
-	// );      
 	Reg_File reg_file(.clk(clock), 
 		.RegWrite(reg_file__in__RegWrite),
 		.ReadRegister1(reg_file__in__read_addr_1),  
@@ -112,70 +117,138 @@ module pipeline(
 	);
 
 	
-
+	
 	
 
-	wire [31:0] id_ex__in__extended_im;
-	// module ID_EX(input clk,reset,
-	//              input RegDst,MemRead
-	//              ,MemtoReg,MemWrite,ALUSrc
-	//              ,RegWrite,
-	//              input [1:0] ALUOp,
-	//              input [31:0] ReadData1,ReadData2,ExtendedIm,
-	//              input[4:0] ReadRegister1, ReadRegister2
-	//              ,Rt,Rd,
-	//              output reg RegDstEX,MemReadEX
-	//              ,MemtoRegEX,MemWriteEX,ALUSrcEX
-	//              ,RegWriteEX,
-	//              output reg [1:0] ALUOpEX,
-	//              output reg [31:0] ReadData1EX,ReadData2EX
-	//              ,ExtendedImEX,
-	//              output reg[4:0] ReadRegister1EX
-	//              ,ReadRegister2EX,RtEX,RdEX
-	//              );
+	wire	
+			id_ex__in__RegDst,
+			id_ex__in__MemRead,
+			id_ex__in__MemtoReg,
+			id_ex__in__MemWrite,
+			id_ex__in__ALUSrc,
+			id_ex__in__RegWrite,
+			id_ex__out__RegDst,
+			id_ex__out__MemRead,
+			id_ex__out__MemtoReg,
+			id_ex__out__MemWrite,
+			id_ex__out__ALUSrc,
+			id_ex__out__RegWrite;
+	wire [1:0] 	
+			id_ex__in__ALUOp,
+			id_ex__out__ALUOp;
+	wire [31:0] 	
+			id_ex__in__ExtendedIm, 
+			id_ex__in__ReadData1, 
+			id_ex__in__ReadData2,
+			id_ex__out_ExtendedIm,
+			id_ex__out__ReadData1,
+			id_ex__out__ReadData2;
+	wire [4:0] 	
+			id_ex__in__ReadRegister1,
+			id_ex__in__ReadRegister2,
+			id_ex__in__Rt,
+			id_ex__in__Rd,
+			id_ex__out__ReadRegister1,
+			id_ex__out__ReadRegister2,
+			id_ex__out__Rt,
+			id_ex__out__Rd;
 	ID_EX id_ex(.clk(clock), .reset(reset),
-	        //      input RegDst,MemRead
-	        //      ,MemtoReg,MemWrite,ALUSrc
-	        //      ,RegWrite,
-	        //      input [1:0] ALUOp,
-	        //      input [31:0] ReadData1,ReadData2,ExtendedIm,
-	        //      input[4:0] ReadRegister1, ReadRegister2
-	        //      ,Rt,Rd,
-	        //      output reg RegDstEX,MemReadEX
-	        //      ,MemtoRegEX,MemWriteEX,ALUSrcEX
-	        //      ,RegWriteEX,
-	        //      output reg [1:0] ALUOpEX,
-	        //      output reg [31:0] ReadData1EX,ReadData2EX
-	        //      ,ExtendedImEX,
-	        //      output reg[4:0] ReadRegister1EX
-	        //      ,ReadRegister2EX,RtEX,RdEX
+	        .RegDst(id_ex__in__RegDst),
+		.MemRead(id_ex__in__MemRead),
+		.MemtoReg(id_ex__in__MemtoReg),
+		.MemWrite(id_ex__in__MemWrite),
+		.ALUSrc(id_ex__in__ALUSrc),
+		.RegWrite(id_ex__in__RegWrite),
+		.ALUOp(id_ex__in__ALUOp),
+		.ReadData1(id_ex__in__ReadData1),
+		.ReadData2(id_ex__in__ReadData2),
+		.ExtendedIm(id_ex__in__ExtendedIm),
+		.ReadRegister1(id_ex__in__ReadRegister1), 
+		.ReadRegister2(id_ex__in__ReadRegister2),
+		.Rt(id_ex__in__Rt),
+		.Rd(id_ex__in__Rd),
+		.RegDstEX(id_ex__out__RegDst),
+		.MemReadEX(id_ex__out__MemRead),
+		.MemtoRegEX(id_ex__out__MemtoReg),
+		.MemWriteEX(id_ex__out__MemWrite),
+		.ALUSrcEX(id_ex__out__ALUSrc),
+		.RegWriteEX(id_ex__out__RegWrite),
+		.ALUOpEX(id_ex__out__ALUOp),
+	        .ReadData1EX(id_ex__out__ReadData1),
+		.ReadData2EX(id_ex__out__ReadData2),
+	        .ExtendedImEX(id_ex__out_ExtendedIm),
+		.ReadRegister1EX(id_ex__out__ReadRegister1),
+		.ReadRegister2EX(id_ex__out__ReadRegister2),
+		.RtEX(id_ex__out__Rt),
+		.RdEX(id_ex__out__Rd)
 	);
+
+	assign mux_regfile_out_1__out__data=id_ex__in__ReadData1;
+	assign mux_regfile_out_2__out__data=id_ex__in__ReadData2;
 
 	// TODO: 4 mux 
 
+	MUX321 mux_ex_1();
+	MUX321 mux_ex_2();
+	MUX221 mux_ex_3();
+	MUX221 mux_ex_4();
+
 	// TODO: ALU
 
+	ALU alu();
+
 	// TODO: ALU control
+	ALU_Control alu_control();
 
 	// TODO: Forwarding Unit	
 
 
+	wire 		
+			ex_mem__in__MemRead,
+			ex_mem__in__MemtoReg,
+			ex_mem__in__MemWrite,
+			ex_mem__in__RegWrite,
+			ex_mem__out__MemRead,
+			ex_mem__out__MemtoReg,
+			ex_mem__out__MemWrite,
+			ex_mem__out__RegWrite ;	
+	wire [31:0]	
+			ex_mem__in__ALUResult,
+			ex_mem__in__ReadData,
+			ex_mem__out__ALUResult,
+			ex_mem__out__ReadData ;
+	wire [4:0]	
+			ex_mem__in__ReadRegister1,
+			ex_mem__in__ReadRegister2,
+			ex_mem__in__RegisterDst,
+			ex_mem__out__ReadRegister1,
+			ex_mem__out__ReadRegister2,
+			ex_mem__out__RegisterDst ;
 
-
-	// module EX_MEM(input clk,reset,
-	//              input MemRead,MemtoReg,MemWrite
-	//              ,RegWrite,
-	//              input [31:0] ALUResult,ReadData,
-	//              input[4:0] ReadRegister1, ReadRegister2
-	//              ,RegisterDst,
-	
-	//              output reg MemReadM,MemtoRegM,MemWriteM
-	//              ,RegWriteM,
-	//              output reg [31:0] ALUResultM,ReadDataM,
-	//              output reg [4:0] ReadRegister1M, ReadRegister2M
-	//              ,RegisterDstM);
-	EX_MEM ex_mem(
-
+	EX_MEM ex_mem( .clk(clock), .reset(reset),
+			.MemRead(ex_mem__in__MemRead),
+			.MemtoReg(ex_mem__in__MemtoReg),
+			.MemWrite(ex_mem__in__MemWrite),
+			.RegWrite(ex_mem__in__RegWrite),
+	//              input [31:0] 
+			.ALUResult(ex_mem__in__ALUResult),
+			.ReadData(ex_mem__in__ReadData),
+	//              input[4:0] 
+			.ReadRegister1(ex_mem__in__ReadRegister1), 
+			.ReadRegister2(ex_mem__in__ReadRegister2),
+			.RegisterDst(ex_mem__in__RegisterDst),
+	//              output reg 
+			.MemReadM(ex_mem__out__MemRead),
+			.MemtoRegM(ex_mem__out__MemtoReg),
+			.MemWriteM(ex_mem__out__MemWrite),
+			.RegWriteM(ex_mem__out__RegWrite),
+	//              output reg [31:0] 
+			.ALUResultM(ex_mem__out__ALUResult),
+			.ReadDataM(ex_mem__out__ReadData),
+	//              output reg [4:0] 
+			.ReadRegister1M(ex_mem__out__ReadRegister1), 
+			.ReadRegister2M(ex_mem__out__ReadRegister2),
+			.RegisterDstM(ex_mem__out__RegisterDst)
 	);
 
 
